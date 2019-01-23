4d53bee5b65beb1d37451c34c17d96747be5ee3a
Viktor Sukochev <sukochevvv@gmail.com>
2019-01-23 18:22:27 +0700

    Settings: using dynamic cell as options

--- Sources/UI/Settings/SettingsViewController.swift
+++ Sources/UI/Settings/SettingsViewController.swift
@@ -8,12 +8,54 @@
 
 import UIKit
 
+fileprivate struct SettingsOption
+{
+    let cellIdentifier: String
+}
+
 class SettingsViewController: ThemeViewController
 {
+    fileprivate let options = [
+        SettingsOption(cellIdentifier: "theme_cell"),
+        SettingsOption(cellIdentifier: "language_cell"),
+        SettingsOption(cellIdentifier: "legal_cell"),
+        SettingsOption(cellIdentifier: "support_cell")
+    ]
+    
+    @IBOutlet fileprivate weak var tableView: UITableView!
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+        self.tableView.reloadData()
+    }
+    
     // MARK: - Actions
     
     @IBAction func onBack()
     {
         self.dismiss(animated: true, completion: nil)
+    }
+}
+
+extension SettingsViewController: UITableViewDataSource, UITableViewDelegate
+{
+    func numberOfSections(in tableView: UITableView) -> Int
+    {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return self.options.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let option = self.options[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: option.cellIdentifier)!
+        
+        return cell
     }
 }
