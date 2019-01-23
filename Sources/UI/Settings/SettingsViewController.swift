11181a296bc3ca5f0f8e6fcc969ef41f5ebf5ff9
Viktor Sukochev <sukochevvv@gmail.com>
2019-01-23 16:05:30 +0700

    Settings navigation

---
+++ Sources/UI/Settings/SettingsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SettingsViewController.swift
+//  ringoid
+//
+//  Created by Victor Sukochev on 23/01/2019.
+//  Copyright © 2019 Ringoid. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: ThemeViewController
+{
+    // MARK: - Actions
+    
+    @IBAction func onBack()
+    {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
