065fffce75cd50ff272757103740881231675861
Viktor Sukochev <sukochevvv@gmail.com>
2019-02-28 19:45:50 +0700

    Crop view scaling logic

--- Sources/UI/UserProfilePhotos/UserProfilePhotoCropViewController.swift
+++ Sources/UI/UserProfilePhotos/UserProfilePhotoCropViewController.swift
@@ -40,8 +40,8 @@ class UserProfilePhotoCropViewController: BaseViewController
     {
         let contentViewSize = self.contentView.bounds.size
         let scale = self.contentView.zoomScale
-        let scaledCropWidth: CGFloat = 336.0
-        let scaledCropHeight: CGFloat = 336.0
+        let scaledCropWidth: CGFloat = self.view.bounds.width - 32.0
+        let scaledCropHeight: CGFloat = scaledCropWidth * AppConfig.photoRatio
         let scaledContentOffsetX = self.contentView.contentOffset.x
         let scaledContentOffsetY = self.contentView.contentOffset.y
         let scaledCropOffsetX = (contentViewSize.width - scaledCropWidth) / 2.0 + scaledContentOffsetX
@@ -86,25 +86,27 @@ class UserProfilePhotoCropViewController: BaseViewController
         
         let size = image.size
         let boundsSize = self.view.bounds.size
-        let photoAreaSize = boundsSize.width * 0.9
-        let smallerSize = min(size.width, size.height)
-        let defaultScale = boundsSize.width / smallerSize
-        let minScale = photoAreaSize / smallerSize
+        let photoAreaWidth = boundsSize.width - 32.0
+        let photoAreaHeight = photoAreaWidth * AppConfig.photoRatio
+        var defaultScale: CGFloat = 1.0
+        
+        if size.height >= size.width { // Portrait
+            defaultScale = photoAreaWidth / size.width
+        } else { // Landscape
+            defaultScale = photoAreaHeight / size.height
+        }
+        
         let maxScale = defaultScale * 3.0
         
-        let contentViewHeight = size.height * minScale
-        let contentViewWidth = size.width * minScale
-        
-        let contentInsetX = (size.width - smallerSize) * minScale / 2.0
-        let contentInsetY = (size.height - smallerSize) * minScale / 2.0
-        
+        let contentViewHeight = size.height * defaultScale
+        let contentViewWidth = size.width * defaultScale
+
         self.contentHeightConstraint.constant = contentViewHeight
         self.contentWidthConstraint.constant = contentViewWidth
         
-        self.contentView.minimumZoomScale = minScale
+        self.contentView.minimumZoomScale = defaultScale
         self.contentView.maximumZoomScale = maxScale
         self.contentView.contentSize = size
-        self.contentView.contentInset = UIEdgeInsets(top: contentInsetY, left: contentInsetX, bottom: contentInsetY, right: contentInsetX)
         self.contentView.setZoomScale(defaultScale, animated: false)
         let initialCenterOffset = CGPoint(
             x: (self.contentView.contentSize.width - contentViewWidth) / 2.0,
