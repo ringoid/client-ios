8fb50a3abcedd173980267aa3da2bcbf3e7d75b1
Viktor Sukochev <sukochevvv@gmail.com>
2019-03-03 15:42:01 +0700

    appstore link added

--- Sources/AppConfig.swift
+++ Sources/AppConfig.swift
@@ -18,4 +18,5 @@ class AppConfig
     static let termsUrl: URL = URL(string: "http://ringoid.com/terms.html")!
     static let policyUrl: URL = URL(string: "http://ringoid.com/privacy.html")!
     static let licensesUrl: URL = URL(string: "http://ringoid.com/licenses-ios.html")!
+    static let appstoreUrl: URL = URL(string: "https://itunes.apple.com/il/app/speakapp-voice-messenger/id1453136158?mt=8")!
 }
