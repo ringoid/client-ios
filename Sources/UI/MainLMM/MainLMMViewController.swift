f37cd960a1fdf5c1c6ac3de642865442e2344f63
Viktor Sukochev <sukochevvv@gmail.com>
2019-03-25 13:16:24 +0700

    Main bottom bar layout fixes

--- Sources/UI/MainLMM/MainLMMViewController.swift
+++ Sources/UI/MainLMM/MainLMMViewController.swift
@@ -461,7 +461,7 @@ class MainLMMViewController: BaseViewController
             let cellBottomOffset = cellTopOffset + cell.bounds.height
             
             vc.topVisibleBorderDistance = cellTopOffset - contentOffset - self.view.safeAreaInsets.top - 34.0
-            vc.bottomVisibleBorderDistance = tableBottomOffset - cellBottomOffset - self.view.safeAreaInsets.bottom - 62.0
+            vc.bottomVisibleBorderDistance = tableBottomOffset - cellBottomOffset - self.view.safeAreaInsets.bottom - 42.0
         }
         
         // Footer
