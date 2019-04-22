dbad37cb8c24bd3037ad08542805203efb9231bc
Viktor Sukochev <sukochevvv@gmail.com>
2019-04-22 15:23:16 +0700

    Profile: likes views removed

--- Sources/UI/UserProfilePhotos/UserProfilePhotoViewController.swift
+++ Sources/UI/UserProfilePhotos/UserProfilePhotoViewController.swift
@@ -28,8 +28,6 @@ class UserProfilePhotoViewController: UIViewController
     fileprivate let disposeBag: DisposeBag = DisposeBag()
     
     @IBOutlet fileprivate weak var photoView: UIImageView?
-    @IBOutlet fileprivate weak var likeView: UIView?
-    @IBOutlet fileprivate weak var likeLabel: UILabel?
     @IBOutlet fileprivate weak var photoIdLabel: UILabel?
     
     override func viewDidLoad()
@@ -61,32 +59,6 @@ class UserProfilePhotoViewController: UIViewController
     
     fileprivate func updateBindings()
     {
-        self.photo?.rx.observe(UserPhoto.self, "likes").observeOn(MainScheduler.instance).subscribe(onNext: { [weak self] _ in
-            guard let photo = self?.photo else {
-                return
-            }
-            
-            let shadow = NSShadow()
-            shadow.shadowColor = UIColor.black.withAlphaComponent(0.5)
-            shadow.shadowOffset = CGSize(width: 1.0, height: 1.0)
-            shadow.shadowBlurRadius = 1.0
-            
-            let paragraphStyle = NSMutableParagraphStyle()
-            paragraphStyle.alignment = .center
-            
-            let attributes: [NSAttributedString.Key : Any] = [
-                .font: UIFont.systemFont(ofSize: 18.0, weight: .medium),
-                .foregroundColor: UIColor.white,
-                .shadow: shadow,
-                .paragraphStyle: paragraphStyle
-            ]
-            
-            self?.likeLabel?.attributedText = NSAttributedString(string: "\(photo.likes)", attributes: attributes)
-        }).disposed(by: self.disposeBag)
         
-        UIManager.shared.userProfileLikesVisible.asObservable().observeOn(MainScheduler.instance).subscribe(onNext: { [weak self] state in
-            self?.likeView?.isHidden = !state
-            self?.likeLabel?.isHidden = !state
-        }).disposed(by: self.disposeBag)
     }
 }
