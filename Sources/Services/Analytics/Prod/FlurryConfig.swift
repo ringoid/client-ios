5a1f20e92432c1e16dccf7b2b37be823cad456f1
Viktor Sukochev <sukochevvv@gmail.com>
2019-04-23 16:43:28 +0700

    Missed flurry configs

---
+++ Sources/Services/Analytics/Prod/FlurryConfig.swift
@@ -0,0 +1,14 @@
+//
+//  FlurryConfig.swift
+//  ringoid st
+//
+//  Created by Victor Sukochev on 23/04/2019.
+//  Copyright © 2019 Ringoid. All rights reserved.
+//
+
+import Foundation
+
+class FlurryConfig
+{
+    static let key: String = "9YPR9DBV44RS94MHB8T3"
+}
