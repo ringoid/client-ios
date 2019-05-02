4dbf701a72c633dc222a6d363a223e9bede6b5ad
Viktor Sukochev <sukochevvv@gmail.com>
2019-05-02 16:29:50 +0700

    LMHIS: Messages UI in progress

---
+++ Sources/UI/MainLMM/MainLMMMessagesContainerViewController.swift
@@ -0,0 +1,9 @@
+//
+//  MainLMMMessagesContainerViewController.swift
+//  ringoid
+//
+//  Created by Victor Sukochev on 02/05/2019.
+//  Copyright © 2019 Ringoid. All rights reserved.
+//
+
+import Foundation
