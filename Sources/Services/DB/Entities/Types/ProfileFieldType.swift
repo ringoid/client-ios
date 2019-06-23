bd65a20d989602af64d6fe2bac143742f762219a
Viktor Sukochev <sukochevvv@gmail.com>
2019-06-23 15:30:52 +0700

    Hair titles

---
+++ Sources/Services/DB/Entities/Types/ProfileFieldType.swift
@@ -0,0 +1,9 @@
+//
+//  ProfileFieldType.swift
+//  ringoid
+//
+//  Created by Victor Sukochev on 23/06/2019.
+//  Copyright © 2019 Ringoid. All rights reserved.
+//
+
+import Foundation
---
+++ Sources/UI/Settings/Profile/HairTitles.swift
@@ -0,0 +1,9 @@
+//
+//  HairTitles.swift
+//  ringoid
+//
+//  Created by Victor Sukochev on 23/06/2019.
+//  Copyright © 2019 Ringoid. All rights reserved.
+//
+
+import Foundation
