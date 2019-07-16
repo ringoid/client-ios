ac9cc9c1cace2ca5ef12cab135966eea96e25753
Viktor Sukochev <sukochevvv@gmail.com>
2019-07-16 21:09:08 +0700

    MainLMMVC: top border updated

--- Sources/UI/MainLMM/MainLMMViewController.swift
+++ Sources/UI/MainLMM/MainLMMViewController.swift
@@ -670,7 +670,7 @@ class MainLMMViewController: BaseViewController
             let cellTopOffset = CGFloat(index) * cell.bounds.height
             let cellBottomOffset = cellTopOffset + cell.bounds.height
             
-            vc.topVisibleBorderDistance = cellTopOffset - contentOffset - self.view.safeAreaInsets.top - 40.0
+            vc.topVisibleBorderDistance = cellTopOffset - contentOffset - self.view.safeAreaInsets.top - 72.0
             vc.bottomVisibleBorderDistance = tableBottomOffset - cellBottomOffset - self.view.safeAreaInsets.bottom - 72.0
         }
     }
