1f43e5ee85353ec87bb94f3259b47ed33266fb89
Viktor Sukochev <sukochevvv@gmail.com>
2019-08-23 17:17:41 +0700

    SettingsProfileStatusCell: 30 character limitation

--- Sources/UI/Settings/Views/SettingsProfileStatusCell.swift
+++ Sources/UI/Settings/Views/SettingsProfileStatusCell.swift
@@ -10,5 +10,30 @@ import UIKit
 
 class SettingsProfileStatusCell: SettingsProfileFieldCell
 {
-
+    @IBOutlet fileprivate var limitLabel: UILabel!
+    
+    override var valueText: String?
+    {
+        didSet {
+            guard let text = self.valueText else {
+                self.limitLabel.text = "0/30"
+                
+                return
+            }
+            
+            self.limitLabel.text = "\(text.count)/30"
+        }
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool
+    {
+        guard let text = textField.text else { return true }
+        
+        let result = (text as NSString).replacingCharacters(in: range, with: string)
+        if result.count > 30 { return false }
+        
+        self.limitLabel.text = "\(result.count)/30"
+        
+        return true
+    }
 }
