84f347ba2fb6e0109c0fc5da1f9f6a2d25b0c8fb
Viktor Sukochev <sukochevvv@gmail.com>
2019-09-19 20:22:33 +0700

    RateUs: stars area gesture recognizers

--- Sources/UI/RateUs/RateUsViewController.swift
+++ Sources/UI/RateUs/RateUsViewController.swift
@@ -10,6 +10,29 @@ import UIKit
 
 class RateUsViewController: BaseViewController
 {
+    @IBOutlet fileprivate weak var star1ImageView: UIImageView!
+    @IBOutlet fileprivate weak var star2ImageView: UIImageView!
+    @IBOutlet fileprivate weak var star3ImageView: UIImageView!
+    @IBOutlet fileprivate weak var star4ImageView: UIImageView!
+    @IBOutlet fileprivate weak var star5ImageView: UIImageView!
+    @IBOutlet fileprivate weak var panelView: UIView!
+    
+    var starsView: [UIImageView] = []
+    var rating: Int? = nil
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+        self.starsView = [
+            self.star1ImageView,
+            self.star2ImageView,
+            self.star3ImageView,
+            self.star4ImageView,
+            self.star5ImageView,
+        ]
+    }
+    
     override func updateTheme() {}
     
     // MARK: - Actions
@@ -24,6 +47,30 @@ class RateUsViewController: BaseViewController
         self.moveToAppstore()
     }
     
+    @IBAction func tapAction(_ recognizer: UITapGestureRecognizer)
+    {
+        guard let barView = recognizer.view else { return }
+        
+        let location = recognizer.location(in: barView)
+        let width = self.panelView.bounds.width
+        let index = Int(location.x / (width / 5.0))
+        self.rating = index + 1
+        
+        self.updateStars(index)
+    }
+    
+    @IBAction func panAction(_ recognizer: UIPanGestureRecognizer)
+    {
+        guard let barView = recognizer.view else { return }
+        
+        let location = recognizer.location(in: barView)
+        let width = self.panelView.bounds.width
+        let index = Int(location.x / (width / 5.0))
+        self.rating = index + 1
+        
+        self.updateStars(index)
+    }
+    
     // MARK: -
     
     fileprivate func moveToAppstore()
@@ -32,5 +79,19 @@ class RateUsViewController: BaseViewController
         guard let writeReviewURL = URL(string: urlStr) else { return }
         
         UIApplication.shared.open(writeReviewURL)
+    }
+    
+    fileprivate func updateStars(_ index: Int)
+    {
+        let emptyStarImage = UIImage(named: "rate_us_star_empty")
+        let selectedStarImage = UIImage(named: "rate_us_star_selected")
+        
+        for (i, starView) in self.starsView.enumerated() {
+            if i <= index {
+                starView.image = selectedStarImage
+            } else {
+                starView.image = emptyStarImage
+            }
+        }
     }
 }
